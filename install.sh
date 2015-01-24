b1beb0c7abbdde5f8537e7e7d2ff55b00e436077
TobiasBerg <tb@tobiasberg.net>
2015-01-24 15:04:26 +0000

    Create .vim folder if it doesn't exist

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 set -ex
 
+mkdir -p $HOME/.vim;
+
 if [[ ! -d $HOME/.vim/bundle/neobundle.vim ]]; then
   echo "Installing neobundle..."
   git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
