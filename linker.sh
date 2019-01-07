354682529aa3076d463ac03c0bd6b827424a71ea
Tobias Berg <tb@tobiasberg.net>
2019-01-07 12:13:16 +0100

    Linker script is now more safe to use and uses md5 instead of sha256sum

--- linker.sh
+++ linker.sh
@@ -6,24 +6,33 @@ function symlink {
   ln -nsf $1 $2
 }
 
-for file in home/.[^.]*; do
-  path="$(pwd)/$file"
-  base=$(basename $file)
-  target="$HOME/$(basename $file)"
+function linkFiles {
+  for file in home/.[^.]*; do
+    path="$(pwd)/$file"
+    base=$(basename $file)
+    target="$HOME/$(basename $file)"
 
-  if [[ -h $target && ($(readlink $target) == $path)]]; then
-    echo -e "\x1B[90m~/$base is symlinked to your dotfiles.\x1B[39m"
-  elif [[ -f $target && $(sha256sum $path | awk '{print $2}') == $(sha256sum $target | awk '{print $2}') ]]; then
-    echo -e "\x1B[32m~/$base exists and was identical to your dotfile.  Overriding with symlink.\x1B[39m"
-    symlink $path $target
-  elif [[ -a $target ]]; then
-    read -p "\x1B[33m~/$base exists and differs from your dotfile. Override?  [yn]\x1B[39m" -n 1
+    if [[ -h $target && ($(readlink $target) == $path)]]; then
+      echo -e "\x1B[90m~/$base is symlinked to your dotfiles.\x1B[39m"
+    elif [[ -f $target && $(md5 $path | awk '{print $4}') == $(md5 $target | awk '{print $4}') ]]; then
+      echo -e "\x1B[32m~/$base exists and was identical to your dotfile.  Overriding with symlink.\x1B[39m"
+      symlink $path $target
+    elif [[ -a $target ]]; then
+      read -p "\x1B[33m~/$base exists and differs from your dotfile. Override?  [yn]\x1B[39m" -n 1
 
-    if [[ $REPLY =~ [yY]* ]]; then
+      if [[ $REPLY =~ [yY]* ]]; then
+        symlink $path $target
+      fi
+    else
+      echo -e "\x1B[32m~/$base does not exist. Symlinking to dotfile.\x1B[39m"
       symlink $path $target
     fi
-  else
-    echo -e "\x1B[32m~/$base does not exist. Symlinking to dotfile.\x1B[39m"
-    symlink $path $target
-  fi
-done
+  done
+}
+
+if hash md5 2>/dev/null; then
+  linkFiles
+else 
+  echo "md5 is not installed"
+  exit 2  
+fi
